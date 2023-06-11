3c6c006a7e1457ba866d88461b68cee1ff2ac9f6
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-06-11 20:05:36 +0100

    this is the structure that i am gonna follow

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="section skills span3">
+        Skills
+      </div>
+
+      <div className="section">
+        About
+      </div>
+
+      <div className="section">
+        Education
+      </div>
+
+      <div className="section">
+        Experience
+      </div>
+
+      <div className="section span3">
+        Projects
+      </div>
+
+      <div className="section span3">
+        Informations
+      </div>
+    </div>
+  )
+
+}
+
+export default App
